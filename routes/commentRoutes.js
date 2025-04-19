16a0b64d02e0fc335cd4dcc08a95d1033e7a2f52
Yusuf Martinus Arief <brothanes11@gmail.com>
2025-04-19 20:28:18 +0700

    add: crud sederhana pada notification controller & end point pada router

---
+++ routes/commentRoutes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    addComment,
+    getCommentFromCache,
+    deleteCommentFromCache,
+    getAllComments,
+    getCommentById,
+    updateComment,
+    deleteComment,
+} = require('../controller/commentController');
+
+// Create a new comment
+router.post('/comments', addComment);
+
+// Get all comments
+router.get('/comments', getAllComments);
+
+// Get a comment by ID
+router.get('/comments/:id', getCommentById);
+
+// Update a comment
+router.put('/comments/:id', updateComment);
+
+// Delete a comment
+router.delete('/comments/:id', deleteComment);
+
+// Get comment from cache
+router.get('/comments/cache/:id', getCommentFromCache);
+
+// Delete comment from cache
+router.delete('/comments/cache/:id', deleteCommentFromCache);
+
+module.exports = router;
